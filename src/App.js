2be58607121264d9b25fd985ef71d9d04d45d29e
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-03-09 18:43:35 +0100

    fikset mer på map() og endret navn på movie.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './css/main.css';
 import Layout from './components/Layout';
 import Main from './components/Main';
-import Movies from './components/Movies';
+import MovieCard from './components/MovieCard';
 function App() {
   return (
     <>
     <Layout>
-      <Movies/>
+      <MovieCard/>
     </Layout>
     </>
   )
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Main from './Main';
-export default function Movies() {
+export default function MovieCard() {
     const [movies, setMovies] = useState([]);
   
     const bondMovies = async () => {
-      const response = await fetch(`http://www.omdbapi.com/?s=James+Bond&apikey=c0e51189`);
+     const response = await fetch('http://www.omdbapi.com/?s=star+wars&type=movie&apikey=c0e51189');
       const data = await response.json();
       const movieArray = data.Search; 
       setMovies(movieArray);
