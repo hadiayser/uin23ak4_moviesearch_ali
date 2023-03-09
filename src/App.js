6c57641130c577c8f346afc3c0e8137b73003185
hadiayser <111729440+hadiayser@users.noreply.github.com>
2023-03-09 15:50:25 +0100

    Fikset 10 filmer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './css/main.css';
 import Layout from './components/Layout';
-
+import Main from './components/Main';
+import Movies from './components/Movies';
 function App() {
   return (
     <>
-    <Layout/>
+    <Layout>
+      <Movies/>
+    </Layout>
     </>
   )
    
   
 }
-
 export default App;
